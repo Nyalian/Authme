3bdd51322f035303eb50e90da87953b83046b264
Nyalian <550162917@qq.com>
2023-04-04 16:54:56 +0800

    Initial commit

---
+++ src/main/java/online/nyalian/authme/Authme.java
@@ -0,0 +1,17 @@
+package online.nyalian.authme;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Authme extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
